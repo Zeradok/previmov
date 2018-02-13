0b84f1f24c9cb033c3bb34e451aa345f806c59d6
Regis A. Ely <regisaely@gmail.com>
2018-02-13 11:33:55 -0200

    add url

--- imoveis.py
+++ imoveis.py
@@ -5,7 +5,8 @@ from unicodedata import normalize
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.common.exceptions import WebDriverException   
+from selenium.common.exceptions import WebDriverException
+
 
 def getimov(pags=10, cidade="pelotas", todas=False):
     firefox_capabilities = DesiredCapabilities.FIREFOX
@@ -13,15 +14,17 @@ def getimov(pags=10, cidade="pelotas", todas=False):
     driver = webdriver.Firefox(capabilities=firefox_capabilities)
     url = 'http://www.zapimoveis.com.br/venda/imoveis/rs+' + cidade
     driver.get(url)
-    data = [['preco', 'quartos', 'suites', 'vagas', 'area', 'bairro', 'tipo', 'cidade']]
+    data = [['preco', 'quartos', 'suites', 'vagas', 'area', 'bairro', 'tipo',
+             'cidade']]
     pag = 1
     while pag <= pags:
         sleep(5)
         pag_url = driver.page_source
         soup = BeautifulSoup(pag_url, 'html.parser')
         page = soup.find(class_='pagination').input['value']
-        if todas == True:
-            pags = int(soup.find(class_='pull-right num-of').get_text().split()[1])
+        if todas:
+            pags = soup.find(class_='pull-right num-of').get_text().split()[1]
+            pags = int(pags)
         aux = 0
         if int(page) != pag:
             aux += 1
@@ -36,7 +39,8 @@ def getimov(pags=10, cidade="pelotas", todas=False):
             endereco = enderecos[i]
             data_row = []
             price = imovel.find_all(class_='preco')[0].get_text()
-            data_row.append(re.sub(r'R\$|\.', '', price).strip().split('\n')[0])
+            data_row.append(re.sub(r'R\$|\.', '',
+                                   price).strip().split('\n')[0])
             try:
                 quartos = imovel.find_all(class_='icone-quartos')[0].get_text()
                 if re.match(r'^.*?(\d*)\squarto.*', quartos):
@@ -48,7 +52,8 @@ def getimov(pags=10, cidade="pelotas", todas=False):
             try:
                 suites = imovel.find_all(class_='icone-suites')[0].get_text()
                 if re.match(r'^.*?(\d*)\ssu\xedte.*', suites):
-                    data_row.append(re.sub(r'^.*?(\d*)\ssu\xedte.*', r'\1', suites))
+                    data_row.append(re.sub(r'^.*?(\d*)\ssu\xedte.*', r'\1',
+                                           suites))
                 else:
                     data_row.append(u'0')
             except IndexError:
@@ -79,8 +84,12 @@ def getimov(pags=10, cidade="pelotas", todas=False):
                 data_row.append(tipo)
             except IndexError:
                 data_row.append(u'')
-            row = [normalize('NFKD', x).encode('ascii', 'ignore') for x in data_row]
+            row = [normalize('NFKD', x).encode('ascii',
+                                               'ignore') for x in data_row]
             row.append(cidade)
+            imov_url = re.search("(?P<url>https?://[^\s]+)",
+                                 str(imovel)).group("url").split("\"")[0]
+            row.append(imov_url)
             data.append(row)
         if pag != pags:
             btn_next = driver.find_element_by_id('proximaPagina')
@@ -92,6 +101,7 @@ def getimov(pags=10, cidade="pelotas", todas=False):
         pag += 1
     driver.close()
     return data
+
 
 def savecsv(data, dest='imoveis.csv'):
     with open(dest, 'wb') as f:
